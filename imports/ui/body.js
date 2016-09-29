31f6b78824714fdd83f659559c53a72795f985d8
Tucker McLean <me@tuckermclean.com>
2016-09-28 21:42:21 -0500

    Can add articles now

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,29 @@ import './body.html';
 
 Template.body.helpers({
   articles() {
-    return Articles.find({});
+    return Articles.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-article'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const title = target.title.value;
+    const content = target.content.value;
+
+    // Insert a task into the collection
+    Articles.insert({
+      title,
+      content,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.title.value = '';
+    target.content.value = '';
   },
 });
